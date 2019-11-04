3e54af0886ab8f60d9feb2f889c39a7af52661bc
Bart Otten <botten@computest.nl>
2019-11-04 11:39:48 +0100

    Update hackney version to fix HTTPS key retrieval on Erlang 22

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule OAuth2.Mixfile do
 
   defp deps do
     [
-      {:hackney, "~> 1.13"},
+      {:hackney, "~> 1.15 and >= 1.15.2"},
 
       # Test dependencies
       {:jason, "~> 1.0", only: [:dev, :test]},
